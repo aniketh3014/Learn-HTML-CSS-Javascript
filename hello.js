e600ea8e770926ed5d0280fb47306ea9efdeb046
Aniket <ghoshaniket1000@gmail.com>
2023-07-12 15:43:17 +0530

    Greet massage modified

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded',function(){
     document.querySelector('form').addEventListener('submit', function(event){
         let name=document.querySelector('#name').value;
-        alert('thanks for visiting. have a great day, '+name);
+        alert('Thanks for visiting! have a great day, '+name);
         event.preventDefault();
     });
 });
